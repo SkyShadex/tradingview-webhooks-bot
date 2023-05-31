34a477e1b2dfc927bd6a37e42fce870e0195bd19
SkyShadex <116922168+SkyShadex@users.noreply.github.com>
2023-05-31 19:18:06 -0400

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
 API_KEY = 'youralpacaapikey'
 API_SECRET = 'youralpacaapisecret'
 WEBHOOK_PASSPHRASE = 'yourpassphrase'
-LOCAL_HOST= '0.0.0.0'
+LOCAL_HOST = '0.0.0.0'
+PORT = '5000'
 DISCORD_WEBHOOK_URL = False # use a string containing your discord webhook url to enable
